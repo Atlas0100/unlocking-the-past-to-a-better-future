ce6548eb546d1bfbc414ba764d0ef175d7065505
Atlas0100 <154034722+Atlas0100@users.noreply.github.com>
2023-12-17 10:49:03 +0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,119 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Hello World":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . 8 9 8 8 9 9 . . . . . 
+. . . . 8 . f f f f . 9 . . . . 
+. . . 8 . f b b b 1 f . 1 . . . 
+. . 8 . f b b b b b 1 f . 1 . . 
+. 8 . f b b f f f f b 1 f . 9 . 
+. 8 . f b f f d d f f b f . 9 . 
+. 8 . b f d f d d f d f b . 8 . 
+. 8 . b f d 7 d d 7 d f b . 9 . 
+. . 8 . . f d d d d f . . 8 . . 
+. . . 8 f 9 f f f f 9 f 8 . . . 
+. . e 4 f 9 9 9 9 9 9 f 4 e . . 
+. . 4 d f 9 9 9 9 9 9 f d 4 . . 
+. . 4 4 f 6 6 7 7 6 6 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image2":
+            case "L Swim":return img`
+. . . . . . . . . . . . . . . . 
+. . . 9 9 8 9 1 . . . . . . . . 
+. . 1 . . . . . 8 . . . . . . . 
+. 1 . f f f f f . 8 . . . . . . 
+9 . f b b 1 b b f . 8 . . . . . 
+9 f b b 1 b b b f . . f f f f . 
+8 f b 1 b b b b f . f 4 d d f f 
+8 f b b b b b d d f 9 f f f f f 
+8 f b b b d d d d f 9 9 9 6 f f 
+9 f b b b f f 7 d f 9 9 9 6 f . 
+8 . f b b d d d f . f f f 7 f . 
+. 8 . f f f f f . 8 . . . . . . 
+. . 8 . . . . . 8 . . . . . . . 
+. . . 8 8 8 8 8 . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "R Swim":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . 1 9 8 9 9 . . . 
+. . . . . . . 8 . . . . . 1 . . 
+. . . . . . 8 . f f f f f . 1 . 
+. . . . . 8 . f b b 1 b b f . 9 
+. f f f f . . f b b b 1 b b f 9 
+f f d d 4 f . f b b b b 1 b f 8 
+f f f f f 9 f d d b b b b b f 8 
+f f 6 9 9 9 f d d d d b b b f 8 
+. f 6 9 9 9 f d 7 f f b b b f 9 
+. f 7 f f f . f d d d b b f . 8 
+. . . . . . 8 . f f f f f . 8 . 
+. . . . . . . 8 . . . . . 8 . . 
+. . . . . . . . 8 8 8 8 8 . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "Text Box":return img`
+6 6 6 6 6 6 6 6 6 6 6 6 7 2 f 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "mySong":return hex`00780004080200`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
